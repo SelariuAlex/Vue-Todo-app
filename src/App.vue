6ad9b0a597e668968c827ab1d0e7936a8b21e5cb
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-01-15 12:47:11 +0200

    vue todo app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <Header/>
+      <AddTodo v-on:add-todo="addTodo"/>
+      <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from "./components/Todos";
+import Header from "./components/Header";
+import AddTodo from "./components/AddTodo";
+import uuid from "uuid";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Header,
+    Todos,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: uuid.v4(),
+          title: "Learn Vue",
+          completed: false
+        },
+        {
+          id: uuid.v4(),
+          title: "Learn React",
+          completed: false
+        },
+        {
+          id: uuid.v4(),
+          title: "Learn Angular",
+          completed: false
+        }
+      ]
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+
+.container {
+  padding: 5vw 15vw;
+}
+
+@media only screen and (max-width: 768px) {
+  .container {
+    padding: 0 5px;
+  }
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header">
+    <h1>Vue TudoList</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+.header {
+  background: #42b883;
+  color: #35495e;
+  text-align: center;
+  padding: 10px;
+}
+</style>
